2a217a86ebf2f29a45d7e924845e25e6e31a26a1
Ram Paranjothy <ram.paranjothy@amd.com>
2020-10-16 01:17:11 -0500

    code clean up

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 function setup() {
   createCanvas(800, 800);
-  dx = 15;
+  dx = 25;
   w = width / dx;
   r = [];
   c = [];
@@ -9,20 +9,14 @@ function setup() {
   for (i = 0; i <= dx; i++) {
     for (j = 0; j <= dx; j++) {
       stroke(0);
-      // strokeWeight(2);
       [x, y] = [i * w, j * w];
       push();
       translate(x, y);
-      // rotate(Math.PI / 4);
       rect(0, 0, w, w);
       pop();
-      // e = parseFloat(random().toFixed(2));
       v = random();
-
-      // text(v, w * j + 10, w * i);
       r.push(v);
     }
-    // console.table(r);
     c.push(r);
     r = [];
   }
