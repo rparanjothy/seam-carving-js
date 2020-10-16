415c1b35a49d8ec59754c124c5404a34548ffa47
Ram Paranjothy <ram.paranjothy@amd.com>
2020-10-16 01:12:04 -0500

    added animate

--- sketch.js
+++ sketch.js
@@ -1,43 +1,96 @@
 function setup() {
   createCanvas(800, 800);
-  dx = 15;
+  dx = 55;
   w = width / dx;
   r = [];
   c = [];
   console.log(w);
 
+  frameRate(12);
+  // for (i = 0; i <= dx; i++) {
+  //   for (j = 0; j <= dx; j++) {
+  //     stroke("#00111111");
+  //     [x, y] = [i * w, j * w];
+  //     push();
+  //     translate(x, y);
+  //     rect(0, 0, w, w);
+  //     pop();
+  //     v = random();
+  //     r.push(v);
+  //   }
+  //   c.push(r);
+  //   r = [];
+  // }
+
+  // //
+  // noStroke();
+  // fill("#ff0000ff");
+  // c.forEach((y, idx) => {
+  //   if (idx == 0) {
+  //     minIdx = y.findIndex((x) => x === Math.min(...y));
+  //     markMin(minIdx, idx);
+  //   }
+
+  //   // next line
+  //   if (idx > 0) {
+  //     fill("#00ff00af");
+
+  //     if (minIdx > 0 && minIdx < y.length) {
+  //       nextRowMin = Math.min(y[minIdx - 1], y[minIdx], y[minIdx + 1]);
+  //     }
+  //     if (minIdx == 0) {
+  //       nextRowMin = Math.min(y[minIdx], y[minIdx + 1]);
+  //     }
+  //     if (minIdx == y.length - 1) {
+  //       nextRowMin = Math.min(y[minIdx - 1], y[minIdx]);
+  //     }
+  //     minIdx = y.findIndex((e) => e === nextRowMin);
+  //     markMin(minIdx, idx);
+  //   }
+  // });
+}
+
+function markMin(xidx, yidx, radius) {
+  ellipse(xidx > 0 ? xidx * w - 15 : w - 15, yidx > 0 ? yidx * w + 15 : 15, 10);
+}
+
+function draw() {
+  c = [];
+  // background(220);
+  // fill(0);
+  strokeWeight(1);
+
   for (i = 0; i <= dx; i++) {
     for (j = 0; j <= dx; j++) {
-      stroke(0);
-      // strokeWeight(2);
+      stroke("#00111111");
       [x, y] = [i * w, j * w];
       push();
       translate(x, y);
-      // rotate(Math.PI / 4);
       rect(0, 0, w, w);
       pop();
-      // e = parseFloat(random().toFixed(2));
       v = random();
-
-      // text(v, w * j + 10, w * i);
       r.push(v);
+      // i > 5 && i < 30 && j > 5 && j < 30 ? r.push(2) : r.push(v);
     }
-    // console.table(r);
     c.push(r);
     r = [];
   }
 
   //
   noStroke();
-  fill("#ff0000ff");
+
   c.forEach((y, idx) => {
     if (idx == 0) {
       minIdx = y.findIndex((x) => x === Math.min(...y));
+      push();
+      fill("#ff0000ff");
       markMin(minIdx, idx);
+      pop();
     }
 
     // next line
     if (idx > 0) {
+      push();
       fill("#00ff00af");
 
       if (minIdx > 0 && minIdx < y.length) {
@@ -51,14 +104,7 @@ function setup() {
       }
       minIdx = y.findIndex((e) => e === nextRowMin);
       markMin(minIdx, idx);
+      pop();
     }
   });
-}
-
-function markMin(xidx, yidx, radius) {
-  ellipse(xidx > 0 ? xidx * w - 15 : w - 15, yidx > 0 ? yidx * w + 15 : 15, 15);
-}
-
-function draw() {
-  // background(220);
 }
